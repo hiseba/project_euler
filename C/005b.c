6b7b7da559ab5388f45ba4f1dcb432b616b75171
hiseba <hiseba@yandex.com>
2013-10-10 22:22:52 +0200

    Update 005b.c

--- C/005b.c
+++ C/005b.c
@@ -1,24 +1,33 @@
-// Smallest multiple
-// What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20
+//  Smallest multiple
+//  What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20
+//  Second version
 
 #include <stdio.h>
 #define N 20
 
-int main(){
-int arr[]={11,12,13,14,15,16,17,18,19,20};
-int found = 0;
-int number = 1;
-int i;
-while (found!=10){
-found = 0;
-number++;
-for (i=0;i<10;i++)
-{
-if(number%arr[i]==0)	found+=1;
+int GCD(int a, int b){
+  int c;
+  while(a!=0){
+    c = a;
+    a = b%a;
+    b = c;
+  };
+  return b;
 }
-//printf(": %d - %d\n",number,found);
-};
-printf("The number is: %d\n",number);
-return 0;
+
+int LCM(int a, int b){
+  return a/GCD(a,b)*b;
+}
+
+int main(int argc,char **argv){
+  int number = 1;
+  int i;
+  
+  for (i=2;i<N;i++)
+    {
+      number = LCM(number,i);
+    }
+  printf("The number is: %d\n",number);
+  return 0;
 }
 
